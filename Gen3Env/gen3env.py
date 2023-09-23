8dbba93eb484e87b50ca191a53a22bde0a6312cc
ZDDWLIG <88024901+ZDDWLIG@users.noreply.github.com>
2023-09-23 22:09:21 +0800

    Update and rename gen3env.py to Gen3Env/gen3env.py

--- Gen3Env/gen3env.py
+++ Gen3Env/gen3env.py
@@ -11,6 +11,7 @@ import geometry_msgs.msg
 import numpy as np
 import gym.spaces as spaces
 from robot import Robot
+from retry import retry
 # TASK_LIST={
 #     'peg_in_hole':
 # }
@@ -20,8 +21,8 @@ class gen3env(gym.Env):
     def __init__(self):
         super(gen3env,self).__init__()
         self.robot=Robot()
-        self.peg_pose=[-0.2,0,0.3]  #[0.1,0,0.3] 
-        self.hole_pose=[0.7,-0.2,0.3] #[1.,-0.2,0.3] 
+        self.peg_pose=[0.45,0,0.3]  #[0.1,0,0.3] 
+        self.hole_pose=[0.8,-0.2,0.3] #[1.,-0.2,0.3] 
         self.info=None
         self.fail_num=0
         self.step_num=0
@@ -32,6 +33,7 @@ class gen3env(gym.Env):
         self.action_space=spaces.Box(low=np.array(action_low),high=np.array(action_high),dtype=np.float64)#end_xyz+gripper
         self.observation_space=spaces.Box(low=np.array(obs_low),high=np.array(obs_high),dtype=np.float64)#end_xyz+gripper+peg_xyz+hole_xyz
         self.tot_steps = 0
+    
 
     def reset(self):
         self.robot.remove_scene()
@@ -40,8 +42,6 @@ class gen3env(gym.Env):
         self.step_num=0
         self.fail_num=0
         self.tot_steps = 0     
-        self.render()
-
         return obs
     #get distance
     def compute_dis(self,obs):
@@ -59,6 +59,25 @@ class gen3env(gym.Env):
 
         return end_peg_dis,peg_hole_dis,grab_dis
         
+    def print_all_pos(self):
+        left_finger_pose=self.robot.get_link_pose('left_inner_finger')
+        right_finger_pose=self.robot.get_link_pose('right_inner_finger')
+        finger_pose=(left_finger_pose+right_finger_pose)/2  
+        peg_pos=self.robot.get_obj_pose('peg')
+        hole_pos=self.robot.get_obj_pose('hole')
+        end_effect_pose=self.robot.get_cartesian_pose()
+        end_x=end_effect_pose.position.x
+        end_y=end_effect_pose.position.y
+        end_z=end_effect_pose.position.z
+        end_pose = (end_x, end_y, end_z)
+        print('end:\t {:.3} {:.3} {:.3}'.format(*end_pose))
+        print('fing:\t {:.3} {:.3} {:.3}'.format(*finger_pose))
+        print('peg:\t {:.3} {:.3} {:.3}'.format(*peg_pos))
+        print('hole:\t {:.3} {:.3} {:.3}'.format(*hole_pos))
+        # input()
+# vrep
+
+
     def do_sim(self,action):
         action,_=self.scale(action=action)
         self.success=self.robot.move_add(pose=action[:-1])
@@ -73,12 +92,15 @@ class gen3env(gym.Env):
         end_z=end_effect_pose.position.z
         gripper_opening=self.robot.get_gripper_position()
         # peg_x,peg_y,peg_z=self.robot.get_obj_pose('peg')
-        peg_x,peg_y,peg_z=self.robot.get_link_pose('link_0')
+        peg_x,peg_y,peg_z=self.robot.get_obj_pose('peg')
+        peg_z += 0.04
         hole_x,hole_y,hole_z=self.robot.get_obj_pose('hole')
         obs=np.array([end_x,end_y,end_z,gripper_opening,peg_x,peg_y,peg_z,hole_x,hole_y,hole_z])
+        print('get_obs')
         return obs 
     
     def compute_reward(self,action,obs):
+        # self.print_all_pos()
         end_peg_dis,peg_hole_dis,grab_dis=self.compute_dis(obs)
         peg_pose=obs[4:7]
         left_finger_pose=self.robot.get_link_pose('left_inner_finger')
@@ -93,9 +115,9 @@ class gen3env(gym.Env):
         
         def reach_reward():
             reach_reward=-grab_dis
-            grab_rew=min(action[3],1)*0.05
-            # if left_finger_pose[2]<0.05:
-                # reach_reward-=(0.05 - left_finger_pose[2]) / 0.05 * 0.05
+            grab_rew=action[3]*0.05 if grab_dis < 0.1 else 0
+            if left_finger_pose[2]<0.1:
+                reach_reward-=(0.1 - left_finger_pose[2]) / 0.1 * 0.14
             return reach_reward,grab_rew
         
         # def pick_reward():
@@ -139,8 +161,10 @@ class gen3env(gym.Env):
         # print('before scale',obs)
         # reward,reach_rew,grab_rew,insert_rew,peg_hole_dis=self.compute_reward(action,obs)
         reward,reach_rew,grab_rew,peg_hole_dis=self.compute_reward(action,obs)
+        if peg_hole_dis>0.8:
+            self.robot.reset_scene()
         self.step_num+=1
-        done=bool(peg_hole_dis<0.005) or self.tot_steps == 70 or self.fail_num == 10     
+        done=bool(peg_hole_dis<0.005) or self.tot_steps == 70 or self.fail_num == 5    
         if done:
             self.step_num=0
             self.fail_num=0
@@ -155,7 +179,7 @@ class gen3env(gym.Env):
             "grab_rew":grab_rew,
             # "insert_rew":insert_rew,
             "peg_hole_dis":peg_hole_dis,
-            "success":done,
+            "success":not done,
             "fail_num":self.fail_num,
             "truncated":truncated
         }
